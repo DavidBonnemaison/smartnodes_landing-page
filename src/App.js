5f1e4495d18c2b3f360bba2ab6e85fb62d15280f
David Bonnemaison <david.bonnemaison@gmail.com>
2017-11-22 18:46:02 +0000

    Add Discord logo and link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Particles from 'react-particles-js';
+import Particles from './Particles';
 import logo from './logo.png';
+import discord from './discord.png';
 import './App.css';
 
 class App extends Component {
@@ -8,127 +9,14 @@ class App extends Component {
     return (
       <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <Particles
-          params={{
-            particles: {
-              number: {
-                value: 100,
-                density: {
-                  enable: true,
-                  value_area: 3000
-                }
-              },
-              color: {
-                value: '#ffffff'
-              },
-              shape: {
-                type: 'circle',
-                stroke: {
-                  width: 3,
-                  color: '#ffe6e6'
-                },
-                polygon: {
-                  nb_sides: 6
-                },
-                image: {
-                  src: 'img/github.svg',
-                  width: 100,
-                  height: 100
-                }
-              },
-              opacity: {
-                value: 0.5,
-                random: false,
-                anim: {
-                  enable: false,
-                  speed: 1,
-                  opacity_min: 0.1,
-                  sync: false
-                }
-              },
-              size: {
-                value: 2,
-                random: true,
-                anim: {
-                  enable: false,
-                  speed: 40,
-                  size_min: 0.1,
-                  sync: false
-                }
-              },
-              line_linked: {
-                enable: true,
-                distance: 150,
-                color: '#ffffff',
-                opacity: 0.4,
-                width: 1
-              },
-              move: {
-                enable: true,
-                speed: 6,
-                direction: 'none',
-                random: false,
-                straight: false,
-                out_mode: 'out',
-                bounce: false,
-                attract: {
-                  enable: false,
-                  rotateX: 600,
-                  rotateY: 1200
-                }
-              }
-            },
-            interactivity: {
-              detect_on: 'canvas',
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: 'grab'
-                },
-                onclick: {
-                  enable: true,
-                  mode: 'push'
-                },
-                resize: true
-              },
-              modes: {
-                grab: {
-                  distance: 200,
-                  line_linked: {
-                    opacity: 1
-                  }
-                },
-                bubble: {
-                  distance: 400,
-                  size: 40,
-                  duration: 2,
-                  opacity: 8,
-                  speed: 3
-                },
-                repulse: {
-                  distance: 200,
-                  duration: 0.4
-                },
-                push: {
-                  particles_nb: 4
-                },
-                remove: {
-                  particles_nb: 2
-                }
-              }
-            },
-            retina_detect: true
-          }}
-          style={{
-            background: '#260059',
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            zIndex: -1
-          }}
-        />
+        <a 
+          href="https://discordapp.com/channels/365789833408413696/365789833408413698"
+          target="_blank"
+          className="App-discord"
+        >
+          <img src={discord} alt="discord" />
+        </a>
+        <Particles />
       </div>
     );
   }
---
+++ src/Particles.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import Particles from 'react-particles-js';
+
+export default () =>
+  <Particles
+    params={{
+      particles: {
+        number: {
+          value: window.innerHeight * window.innerWidth / 4000,
+          density: {
+            enable: true,
+            value_area: 3000
+          }
+        },
+        color: {
+          value: '#ffffff'
+        },
+        shape: {
+          type: 'circle',
+          stroke: {
+            width: 3,
+            color: '#ffe6e6'
+          },
+          polygon: {
+            nb_sides: 6
+          },
+          image: {
+            src: 'img/github.svg',
+            width: 100,
+            height: 100
+          }
+        },
+        opacity: {
+          value: 0.5,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false
+          }
+        },
+        size: {
+          value: 2,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 40,
+            size_min: 0.1,
+            sync: false
+          }
+        },
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: '#ffffff',
+          opacity: 0.4,
+          width: 1
+        },
+        move: {
+          enable: true,
+          speed: 6,
+          direction: 'none',
+          random: false,
+          straight: false,
+          out_mode: 'out',
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200
+          }
+        }
+      },
+      interactivity: {
+        detect_on: 'canvas',
+        events: {
+          onhover: {
+            enable: true,
+            mode: 'grab'
+          },
+          onclick: {
+            enable: true,
+            mode: 'push'
+          },
+          resize: true
+        },
+        modes: {
+          grab: {
+            distance: 200,
+            line_linked: {
+              opacity: 1
+            }
+          },
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4
+          },
+          push: {
+            particles_nb: 4
+          },
+          remove: {
+            particles_nb: 2
+          }
+        }
+      },
+      retina_detect: true
+    }}
+    style={{
+      background: '#260059',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      zIndex: -1
+    }}
+  />
